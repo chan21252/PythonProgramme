f7d110a92fe660b5667cf67df4b13846cb2ec444
chan <chanjobs21252@gmail.com>
2017-04-12 17:38:07 +0800

    密码查询

--- chapter6/_6_1_password.py
+++ chapter6/_6_1_password.py
@@ -6,3 +6,19 @@ PASSWORDS = {
 	'weixin': 'qazwsx',
 	'sinaweibo': '789456123aabbcc'
 };
+
+import sys, pyperclip
+
+if len(sys.argv) < 2:
+	print('Usage: python password.py [account] - copy account password');
+	sys.exit();
+	
+account = sys.argv[1];
+
+if account in PASSWORDS:
+	pyperclip.copy(PASSWORDS[account]);
+	print('Password for ' + account + ' copied to clipboard');
+else:
+	print('There is no account named ' + account);
+
+input("Press <enter>");
